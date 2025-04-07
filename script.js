ecfb5bb7e5808b958eb77c1ebea1c1a207e77da1
shekhartyagi <61544121+shekhu-tech@users.noreply.github.com>
2025-04-07 15:33:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const toggleBtn = document.getElementById("toggleBtn");
       mobileNav.classList.toggle("active");
     });
 function openWhatsApp() {
-        var phoneNumber = "918395913829"; // Apna number daaliye
+        var phoneNumber = "#"; // Apna number daaliye
         var message = "Hello, I am interested in services.";
         var whatsappLink = "https://wa.me/" + phoneNumber + "?text=" + encodeURIComponent(message);
         window.open(whatsappLink, '_blank');
